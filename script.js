a3274260148514b49500d5d7e60d7ccc2c116685
Anfield_Lam <1155258814@link.cuhk.edu.hk>
2026-08-26 01:46:53 +0800

    not final but visually working difficulty and mode buttons

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
+	const labels = document.querySelectorAll(".second-header-container > .right > label")
+  let toggledDifficultyList = false;
+  let toggledModeList = false;
+  labels.forEach((label) => {
+		const select = label.querySelector("select");
+
+		label.addEventListener("click", (event) => {
+			
+		});
+	});
   
-})
+});
