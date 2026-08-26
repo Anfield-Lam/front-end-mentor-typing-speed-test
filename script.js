a8baf24b63faf428920734aa073af8dc4e4efa03
Anfield_Lam <1155258814@link.cuhk.edu.hk>
2026-08-27 00:03:15 +0800

    add click to have result in bar

--- script.js
+++ script.js
@@ -3,11 +3,47 @@ document.addEventListener("DOMContentLoaded", () => {
 	const mode = document.querySelector(".mode");
 	const difficultyList = document.querySelector(".outer-difficulty-list");
 	const modeList = document.querySelector(".outer-mode-list");
+	const options = ["easy", "medium", "hard", "timed", "passage"];
+	const shortforms = {
+		"easy": "Easy",
+		"medium": "Medium",
+    "hard": "Hard",
+		"timed": "Timed (60s)",
+		"passage": "Passage"
+	}
 
 	difficulty.addEventListener("click", () => {
 		difficultyList.classList.toggle("hidden");
 	})
+
 	mode.addEventListener("click", () => {
 		modeList.classList.toggle("hidden");
+	})
+
+	const difficultyListOptions = document.querySelector(".difficulty-list").querySelectorAll("*");
+	const difficultySelected = document.querySelector(".difficulty-selected")
+	const modeListOptions = document.querySelector(".mode-list").querySelectorAll("*")
+	const modeListSelected = document.querySelector(".mode-selected")
+
+	difficultyListOptions.forEach((option) => {
+		option.addEventListener("click", () => {
+			options.forEach(item => {
+				if (option.classList.contains(item)) {
+					difficultySelected.textContent = shortforms[item];
+				}
+			})
+			difficultyList.classList.toggle("hidden");	
+		})
+	})
+
+	modeListOptions.forEach((option) => {
+		option.addEventListener("click", () => {
+			options.forEach(item => {
+				if (option.classList.contains(item)) {
+					modeListSelected.textContent = shortforms[item];
+				}
+			})
+			modeList.classList.toggle("hidden");
+		})
 	})
 });
